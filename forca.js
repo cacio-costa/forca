20e6ec70ed9d8e72482f43ba872c13c4adeac5ad
Cácio Costa <cacio.cjcs@gmail.com>
2025-06-18 11:46:44 -0300

    Finaliza jogo da forca

---
+++ forca.js
@@ -0,0 +1,130 @@
+// --- Elementos do DOM ---
+const palavraSecretaElement = document.getElementById('palavra-secreta');
+const letrasChutadasElement = document.getElementById('letras-chutadas');
+const inputPalpite = document.getElementById('input-palpite');
+const btnChutar = document.getElementById('btn-chutar');
+const mensagemElement = document.getElementById('mensagem');
+const membrosCorpo = document.querySelectorAll('.membro-corpo');
+const btnReiniciar = document.getElementById('btn-reiniciar');
+
+// --- Variáveis do Jogo ---
+const palavras = ["javascript", "programacao", "desenvolvedor", "interface", "algoritmo", "computador", "navegador"];
+let palavraSecreta;
+let letrasCorretas;
+let letrasIncorretas;
+
+function iniciarJogo() {
+    let numeroSorteado = Math.random() * palavras.length;
+    let indice = Math.floor(numeroSorteado);
+
+    palavraSecreta = palavras[indice];
+    letrasCorretas = [];
+    letrasIncorretas = [];
+
+    mensagemElement.textContent = '';
+    letrasChutadasElement.textContent = '';
+
+    for (let i = 0; i < membrosCorpo.length; i++) {
+        membrosCorpo[i].classList.remove('visivel');
+    }
+
+    btnChutar.disabled = false;
+    inputPalpite.disabled = false;
+    btnReiniciar.style.display = 'none';
+    atualizarPalavraNaTela();
+}
+
+function atualizarPalavraNaTela() {
+    let letrasDaPalavraSecreta = palavraSecreta.split('');
+
+    palavraSecretaElement.innerHTML = '';
+    for (let i = 0; i < letrasDaPalavraSecreta.length; i++) {
+        let letraDaPalavra = letrasDaPalavraSecreta[i];
+
+        let letraElement = document.createElement('span');
+        letraElement.classList.add('letra');
+        if (letrasCorretas.includes(letraDaPalavra)) {
+            letraElement.textContent = letraDaPalavra;
+        }     
+
+        palavraSecretaElement.appendChild(letraElement);
+
+    }
+}
+
+function processarChute() {
+    let palpite = inputPalpite.value.toLowerCase();
+
+    inputPalpite.value = '';
+    inputPalpite.focus();
+
+    if (letrasCorretas.includes(palpite) || letrasIncorretas.includes(palpite)) {
+        alert('Você já tentou essa letra!');
+        return;
+    }
+
+    if (palavraSecreta.includes(palpite)) {
+        letrasCorretas.push(palpite);
+        atualizarPalavraNaTela();
+        verificarVitoria();
+    } else {
+        letrasIncorretas.push(palpite);
+        atualizarForca();
+        atualizarLetrasErradas();
+    }
+
+}
+
+function atualizarLetrasErradas() {
+    letrasChutadasElement.textContent = letrasIncorretas.join(', ');
+}
+
+function atualizarForca() {
+    let erros = letrasIncorretas.length;
+
+    if (erros > 0) {
+        let indiceDoMembroParaExibir = erros - 1;
+        membrosCorpo[indiceDoMembroParaExibir].classList.add('visivel');
+        verificarDerrota();
+    }
+}
+
+function verificarVitoria() {
+    let todasAsLetras = palavraSecreta.split('');
+
+    let faltaLetras = false;
+    for (let i = 0; i < todasAsLetras.length; i++) {
+        let letra = todasAsLetras[i];
+
+        if (!letrasCorretas.includes(letra)) {
+            faltaLetras = true;
+        }
+    }
+
+    if (!faltaLetras) {
+        exibirMensagemFinal("Você venceu! Parabéns!!!", "vitoria");
+    }
+}
+
+function verificarDerrota() {
+    let erros = letrasIncorretas.length;
+    
+    if (erros == membrosCorpo.length) {
+        exibirMensagemFinal(`Você perdeu! A paravra era ${palavraSecreta}!!!`, 'derrota');
+    }
+}
+
+function exibirMensagemFinal(msg, tipo) {
+    mensagemElement.textContent = msg;
+    mensagemElement.classList.add(tipo);
+
+    btnChutar.disabled = true;
+    inputPalpite.disabled = true;
+
+    btnReiniciar.style.display = 'block';
+}
+
+btnChutar.onclick = processarChute;
+btnReiniciar.onclick = iniciarJogo;
+
+iniciarJogo();
